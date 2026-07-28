23e675e1b22a85454f46b8caf0b159cd1b5c6417
Geyser Ramírez <geyser.grs@gmail.com>
2026-07-28 09:49:35 -0600

    feat(repository): resolve respository implementation

--- src/features/pokemon/infrastructure/repositories/PokemonRepositoryImpl.ts
+++ src/features/pokemon/infrastructure/repositories/PokemonRepositoryImpl.ts
@@ -1,11 +1,14 @@
 import type { Page } from '../../domain/entities/Page';
 import type { Pokemon } from '../../domain/entities/Pokemon';
-import type { PokemonRepository } from '../../domain/repositories/PokemonListRepository';
+import { PokemonByIdRepository } from '../../domain/repositories/PokemonByIdRepository';
+import type { PokemonListRepository } from '../../domain/repositories/PokemonListRepository';
 import type { PokemonDataSource } from '../datasources/PokemonDataSource';
 import { PageMapper } from '../mappers/PageMapper';
 import { PokemonMapper } from '../mappers/PokemonMapper';
 
-export class PokemonRepositoryImpl implements PokemonRepository {
+export class PokemonRepositoryImpl
+  implements PokemonListRepository, PokemonByIdRepository
+{
   constructor(private readonly pokemonDataSource: PokemonDataSource) {}
 
   async fetchPokemonList(offset: number, limit: number): Promise<Page> {
